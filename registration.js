e85f63bfa84bd61538af86a214c113c8a98ce2e9
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-05-06 03:55:31 +0200

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,15 +1,9 @@
 
     import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js'
-
-    // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js'
-
     // Add Firebase products that you want to use
-    import { getAuth } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js'
+    import { getAuth , EmailAuthprovider, createUserWithEmailAndPassword  } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js'
     import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js'
-    import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-    // ...
 
     // TODO: Replace the following with your app's Firebase project configuration
     const firebaseConfig = {
@@ -25,6 +19,18 @@
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
  const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+const auth = getAuth();
 createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed up 
