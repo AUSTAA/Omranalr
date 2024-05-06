5d7ee373af31a915624de935e273f8c8463498d1
Omranalr <168029210+Omranalr@users.noreply.github.com>
2024-05-06 03:49:47 +0200

    Update registration.js

--- registration.js
+++ registration.js
@@ -13,9 +13,15 @@
 
     // TODO: Replace the following with your app's Firebase project configuration
     const firebaseConfig = {
-      // ...
-    };
-
+    apiKey: "AIzaSyB7YJhtaefEPc9NMzhTBjQC06WmSEja0xc",
+    authDomain: "omran-16f44.firebaseapp.com",
+    databaseURL: "https://omran-16f44-default-rtdb.firebaseio.com",
+    projectId: "omran-16f44",
+    storageBucket: "omran-16f44.appspot.com",
+    messagingSenderId: "598982209417",
+    appId: "1:598982209417:web:dc9cbddd485a1ea52bbb58",
+    measurementId: "G-PGZJ0T555G"
+  };
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
  const auth = getAuth();
