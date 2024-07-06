7291f86f51c059dfa8a65452aca11f1b6c2c2dcd
AUSTAA <168029210+AUSTAA@users.noreply.github.com>
2024-07-06 02:13:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -158,15 +158,15 @@ document.addEventListener('DOMContentLoaded', () => {
             case '2': return 2;
             case '3': return 3;
             case '4': return 4;
-case ‘5’: return 5;
-case ‘6’: return 6;
-case ‘7’: return 7;
-case ‘Q’: return 8;
-case ‘J’: return 9;
-case ‘K’: return 10;
-default: return 0;
-}
-}
+            case '5': return 5;
+            case '6': return 6;
+            case '7': return 7;
+            case 'Q': return 8;
+            case 'J': return 9;
+            case 'K': return 10;
+            default: return 0;
+        }
+   }
 
 function findSummingCards(cards, targetValue) {
     const result = [];
