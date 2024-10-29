30fdae5c395063f0228d72d5af1c976f635d2c02
AUSTAA <168029210+AUSTAA@users.noreply.github.com>
2024-10-29 03:41:45 +0200

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,7 +1,8 @@
+// deck.js
 const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
 const values = ['A', '2', '3', '4', '5', '6', '7', 'Q', 'J', 'K'];
 
-function createDeck() {
+export function createDeck() {
     const deck = [];
     for (let suit of suits) {
         for (let value of values) {
@@ -11,7 +12,7 @@ function createDeck() {
     return deck;
 }
 
-function shuffleDeck(deck) {
+export function shuffleDeck(deck) {
     for (let i = deck.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
