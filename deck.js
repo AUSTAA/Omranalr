cde126beb167dc9f3caa5ef6f356008764144d9f
AUSTAA <168029210+AUSTAA@users.noreply.github.com>
2025-03-15 01:26:30 +0200

    Update rules.js

--- deck.js
+++ deck.js
@@ -1,21 +1,56 @@
-// deck.js
-const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
-const values = ['A', '2', '3', '4', '5', '6', '7', 'Q', 'J', 'K'];
-
-export function createDeck() {
-    const deck = [];
-    for (let suit of suits) {
-        for (let value of values) {
-            deck.push({ value, suit });
-        }
+// قانون الشكبة: إضافة قيمة الشكبة إلى النقاط
+function checkShkba(card) {
+    if (middleCards.length === 0) {
+        const lastCard = cardValueToInt(card.value);
+        collectedCards.push({ value: lastCard, suit: card.suit });
+        alert("شكبـّة! + " + lastCard + " نقطة");
     }
-    return deck;
 }
 
-export function shuffleDeck(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
+// قانون البرميلة والحية
+function checkBarmeelaAndSnake() {
+    // 3 سبعات
+    if (middleCards.filter(card => card.value === "7").length === 3) {
+        alert("برميلة!");
+        player1Score += 1;
     }
-    return deck;
+    // 2 سبعات و3 ستات
+    if (middleCards.filter(card => card.value === "7").length === 2 && middleCards.filter(card => card.value === "6").length === 3) {
+        alert("برميلة!");
+        player1Score += 1;
+    }
+    // 7 ديناري
+    if (middleCards.some(card => card.value === "7" && card.suit === "diamonds")) {
+        alert("🐍");
+        player1Score += 1;
+    }
+}
+
+// قوانين الباجي
+function checkBaji() {
+    // ديناري باجي
+    if (player1Hand.filter(card => card.suit === "diamonds").length === 5) {
+        alert("الديناري باجي");
+        return false;
+    }
+    // البرميلة باجي
+    if (middleCards.filter(card => card.value === "7").length === 2 && middleCards.filter(card => card.value === "6").length === 2) {
+        alert("البرميلة باجي");
+        return false;
+    }
+    // الكارطة باجي
+    if (player1Hand.length === 20) {
+        alert("الكارطة باجي");
+        return false;
+    }
+    return true;
+}
+
+// نهاية الشوط
+function checkEndOfGame() {
+    if (player1Score >= 61 || player2Score >= 61) {
+        alert(player1Score >= 61 ? "اللاعب 1 فاز!" : "اللاعب 2 فاز!");
+        player1Score = 0;
+        player2Score = 0;
+    }
 }
