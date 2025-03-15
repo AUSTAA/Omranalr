c0b94c7a91dd3aa8d97a70c01d00585596ea5e6f
AUSTAA <168029210+AUSTAA@users.noreply.github.com>
2025-03-15 23:46:49 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,73 +1,120 @@
-const suits = ["hearts", "spades", "diamonds", "clubs"];
-const suitSymbols = { hearts: "♥", spades: "♠", diamonds: "♦", clubs: "♣" };
-const values = ["A", "2", "3", "4", "5", "6", "7", "Q", "J", "K"];
-let deck, player1Hand, player2Hand, middleCards, player1Collected, player2Collected;
-let player1Score = 0, player2Score = 0;
-let currentPlayer = 1;
-let roundOver = false;
+const Game = {
+    deck: [],
+    player1Hand: [],
+    player2Hand: [],
+    middleCards: [],
+    player1Collected: [],
+    player2Collected: [],
+    player1Score: 0,
+    player2Score: 0,
+    currentPlayer: 1,
+    roundOver: false,
 
-function initializeGame() {
-    deck = createDeck();
-    shuffleDeck(deck);
-    player1Hand = [];
-    player2Hand = [];
-    middleCards = [];
-    player1Collected = [];
-    player2Collected = [];
-    dealInitialCards();
-    currentPlayer = 1;
-    roundOver = false;
-    updateDisplay();
-}
+    initializeGame: function () {
+        this.deck = Deck.createDeck();
+        Deck.shuffleDeck(this.deck);
+        this.player1Hand = [];
+        this.player2Hand = [];
+        this.middleCards = [];
+        this.player1Collected = [];
+        this.player2Collected = [];
+        this.dealInitialCards();
+        this.currentPlayer = 1;
+        this.roundOver = false;
+        UI.updateDisplay();
+    },
 
-function createDeck() {
-    const deck = [];
-    for (let suit of suits) {
-        for (let value of values) {
-            deck.push({ suit, value });
+    dealInitialCards: function () {
+        for (let i = 0; i < 3; i++) {
+            this.player1Hand.push(this.deck.pop());
+            this.player2Hand.push(this.deck.pop());
         }
+        for (let i = 0; i < 4; i++) {
+            this.middleCards.push(this.deck.pop());
+        }
+    },
+
+    playCard: function (cardIndex, player) {
+        if (this.roundOver) return;
+
+        if ((this.currentPlayer === 1 && player !== 1) || (this.currentPlayer === 2 && player !== 2)) {
+            alert("ليس دورك!");
+            return;
+        }
+
+        const currentHand = this.currentPlayer === 1 ? this.player1Hand : this.player2Hand;
+        const collectedCards = this.currentPlayer === 1 ? this.player1Collected : this.player2Collected;
+
+        if (cardIndex < 0 || cardIndex >= currentHand.length) return;
+
+        const card = currentHand[cardIndex];
+        const cardValue = parseInt(card.value) || 0;
+
+        let matchingCardIndex = this.middleCards.findIndex(c => parseInt(c.value) === cardValue);
+
+        if (matchingCardIndex !== -1) {
+            collectedCards.push(this.middleCards.splice(matchingCardIndex, 1)[0]);
+            collectedCards.push(card);
+        } else {
+            let combinations = this.findSummingCombinations(this.middleCards, cardValue);
+
+            if (combinations.length > 0) {
+                combinations[0].forEach(match => {
+                    let index = this.middleCards.findIndex(c => c.value === match.value && c.suit === match.suit);
+                    if (index !== -1) {
+                        collectedCards.push(this.middleCards.splice(index, 1)[0]);
+                    }
+                });
+                collectedCards.push(card);
+            } else {
+                this.middleCards.push(card);
+            }
+        }
+
+        currentHand.splice(cardIndex, 1);
+
+        Rules.checkShkba(this.middleCards, card, collectedCards, this.currentPlayer === 1 ? this.player1Score : this.player2Score);
+        Rules.checkBarmeela(collectedCards, this.currentPlayer === 1 ? this.player1Score : this.player2Score);
+
+        if (this.player1Hand.length === 0 && this.player2Hand.length === 0) {
+            this.dealNextCards();
+        } else {
+            this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+        }
+
+        UI.updateDisplay();
+    },
+
+    dealNextCards: function () {
+        if (this.deck.length >= 6) {
+            for (let i = 0; i < 3; i++) {
+                this.player1Hand.push(this.deck.pop());
+                this.player2Hand.push(this.deck.pop());
+            }
+        } else {
+            this.endRound();
+        }
+    },
+
+    endRound: function () {
+        this.roundOver = true;
+
+        if (Rules.checkBaji(this.player1Collected, this.player2Collected)) {
+            UI.showMessage("باجي! لا تُحسب النقاط!");
+        } else {
+            if (this.player1Collected.length > this.player2Collected.length) {
+                this.player1Score += 1;
+            } else if (this.player2Collected.length > this.player1Collected.length) {
+                this.player2Score += 1;
+            }
+        }
+
+        if (this.player1Score >= 61 || this.player2Score >= 61) {
+            UI.showMessage(this.player1Score >= 61 ? "اللاعب 1 فاز!" : "اللاعب 2 فاز!");
+            this.player1Score = 0;
+            this.player2Score = 0;
+        }
+
+        this.initializeGame();
     }
-    return deck;
-}
-
-function shuffleDeck(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-}
-
-function dealInitialCards() {
-    for (let i = 0; i < 3; i++) {
-        player1Hand.push(deck.pop());
-        player2Hand.push(deck.pop());
-    }
-    for (let i = 0; i < 4; i++) {
-        middleCards.push(deck.pop());
-    }
-}
-
-function updateDisplay() {
-    renderCards("player1-hand", player1Hand, 1);
-    renderCards("player2-hand", player2Hand, 2);
-    renderCards("middle-cards", middleCards);
-
-    document.getElementById("player1-score").textContent = player1Score;
-    document.getElementById("player2-score").textContent = player2Score;
-}
-
-function renderCards(containerId, cards, player) {
-    const container = document.getElementById(containerId);
-    container.innerHTML = "";
-    cards.forEach((card, index) => {
-        const cardElement = document.createElement("div");
-        cardElement.className = `card ${card.suit}`;
-        cardElement.innerHTML = `
-            <div class="top-left">${card.value}<br>${suitSymbols[card.suit]}</div>
-            <div class="symbol">${suitSymbols[card.suit]}</div>
-            <div class="bottom-right">${card.value}<br>${suitSymbols[card.suit]}</div>
-        `;
-        cardElement.addEventListener("click", () => playCard(index, player));
-        container.appendChild(cardElement);
-    });
-}
+};
